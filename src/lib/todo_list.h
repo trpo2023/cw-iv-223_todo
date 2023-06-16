d97f9c40ab32d6ed7842ed823214f41bba7cff94
dimitry05 <124419917+dimitry05@users.noreply.github.com>
2023-06-17 00:46:40 +0700

    Update todo_list.h

--- src/lib/todo_list.h
+++ src/lib/todo_list.h
@@ -1,1 +1,30 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
+typedef struct
+{
+    char **todo;
+    int size;
+    int *selection;
+    int *color;
+} todo_list;
+
+int add_todo(todo_list *list, char *todo);
+
+int delete_todo(todo_list *list, int number_todo);
+
+int print_todo_list(todo_list *list);
+
+void insert_selection(todo_list *list, int num_todo, int selection);
+
+void insert_color(todo_list *list, int num_todo, int color);
+
+void print_selection();
+
+void print_color();
+
+void menu();
+
+void free_todo_list(todo_list *list);
